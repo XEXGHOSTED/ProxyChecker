73e674fc544d80e7d1b9c4b8808ad2ae8e351fb2
Dominik Heise <github@devkit.online>
2024-02-27 09:44:35 +0100

    Add project files.

---
+++ ProxyChecker/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProxyChecker
+{
+    internal class Program
+    {
+        private static List<ProxyStruct> proxyList;
+        public static List<ProxyStruct> GoodProxyList { get; set; }
+
+        static async Task Main(string[] args)
+        {
+            proxyList = new List<ProxyStruct>();
+
+            if (File.Exists(args[0]))
+            {
+                string[] lines = File.ReadAllLines(args[0]);
+                foreach (string line in lines)
+                {
+                    string[] proxy = line.Split(':');
+                    proxyList.Add(new ProxyStruct { IP = proxy[0], Port = int.Parse(proxy[1]) });
+                }
+            }
+
+            GoodProxyList = new List<ProxyStruct>();
+
+            var tasks = new List<Task>();
+
+            int maxConcurrentTasks = 100; // Adjust this number as needed
+            var semaphore = new SemaphoreSlim(maxConcurrentTasks);
+
+            foreach (ProxyStruct proxy in proxyList)
+            {
+                tasks.Add(Task.Run(async () =>
+                {
+                    await semaphore.WaitAsync();
+
+                    try
+                    {
+                        var result = await Utility.CheckProxyAsync(proxy.IP, proxy.Port);
+                        if (result)
+                        {
+                            lock (GoodProxyList)
+                            {
+                                GoodProxyList.Add(proxy);
+                            }
+                            Utility.Write(string.Format("Proxy [{0}:{1}] Online", proxy.IP, proxy.Port), 6, 214, 160);
+                        }
+                        else
+                        {
+                            Utility.Write(string.Format("Proxy [{0}:{1}] Offline", proxy.IP, proxy.Port), 193, 18, 31);
+                        }
+                    }
+                    finally
+                    {
+                        semaphore.Release();
+                    }
+                }));
+            }
+
+            await Task.WhenAll(tasks);
+            Utility.Write(string.Format("Finished checking {0}/{1}", proxyList.Count, GoodProxyList.Count), 17, 138, 178);
+
+            File.WriteAllText("goodproxies.txt", string.Join("\n", GoodProxyList.ConvertAll(x => x.IP + ":" + x.Port)));
+        }
+    }
+
+    public class ProxyStruct
+    {
+        public string IP { get; set; }
+        public int Port { get; set; }
+    }
+}
---
+++ ProxyChecker/Utility.cs
@@ -0,0 +1,49 @@
+﻿namespace ProxyChecker
+{
+    using System;
+    using System.Net;
+    using System.Net.Sockets;
+    using System.Threading.Tasks;
+
+    public class Utility
+    {
+        public static async Task<bool> CheckProxyAsync(string IP, int Port)
+        {
+            try
+            {
+                IPAddress ipAddress;
+                if (!IPAddress.TryParse(IP, out ipAddress))
+                {
+                    // Handle invalid IP address
+                    return false;
+                }
+
+                IPEndPoint endPoint = new IPEndPoint(ipAddress, Port);
+                using (var client = new TcpClient())
+                {
+                    var connectTask = client.ConnectAsync(endPoint.Address, endPoint.Port);
+                    if (await Task.WhenAny(connectTask, Task.Delay(5000)) == connectTask)
+                    {
+                        // Connection succeeded within the timeout
+                        return true;
+                    }
+                    else
+                    {
+                        // Connection timed out
+                        return false;
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+
+        public static void Write(string Msg, int R, int G, int B)
+        {
+            Console.WriteLine("\u001b[38;2;84;84;84m" + DateTime.Now.ToString("T") + "\u001b[38;2;255;255;255m | " + Msg.Replace("[", $"\u001b[38;2;{R};{G};{B}m").Replace("]", "\u001b[38;2;255;255;255m"));
+        }
+    }
+}
